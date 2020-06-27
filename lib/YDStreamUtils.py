f9272104a96f28f98125fae283a9cb358b9f570e
jmzambon <jeanmarczambon@gmail.com>
2020-06-27 12:18:08 +0200

    initial commit

--- lib/YDStreamUtils.py
+++ lib/YDStreamUtils.py
@@ -196,9 +196,8 @@ def moveFile(file_path, dest_path, filename=None):
     destFilePath = os.path.join(dest_path, fname)
     if xbmcvfs.copy(file_path, destFilePath):
         xbmcvfs.delete(file_path)
-        return True
-
-    return False
+        return destFilePath
+    return ''
 
 
 def getDownloadPath(use_default=None):
